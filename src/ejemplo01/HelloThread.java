29261749cf519de019b2672405dbd07ec6ca9885
PAlejandroQ <ppaabblloo4283@gmail.com>
2023-04-23 16:31:04 -0500

    Actualizaciòn de archivos.

--- src/ejemplo01/HelloThread.java
+++
@@ -1,9 +0,0 @@
-package ejemplo01;
-public class HelloThread extends Thread{
-    public void run() {
-        System.out.println("Hello from a thread03!");
-    }
-    public static void main(String args[]) {
-        (new HelloThread()).start();
-    }
-}
--- src/manejadorHilos/IniciaServidorConHilos.java
+++
@@ -1,7 +0,0 @@
-package manejadorHilos;
-public class IniciaServidorConHilos {
-    public static void main(String[] args) {
-        ServidorDeEcoConHilos server = new ServidorDeEcoConHilos();
-        new Thread(server).start();
-    }
-}
